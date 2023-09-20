e8843cd02734969cdef62aa2efbaca6d2ffb9269
Harsh <202151082@iiitvadodara.ac.in>
2023-09-21 03:46:25 +0530

    added header

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -4,7 +4,9 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-post-create',
-    templateUrl: './post-create.component.html'
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css']
+    
 })
 export class PostCreateComponent {
 
